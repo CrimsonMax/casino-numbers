2e56db13fbcc17ff35bf8b7c0bd222c51276922c
CrimsonMax <61148415+CrimsonMax@users.noreply.github.com>
2022-07-12 21:42:17 +0300

    Done!

--- script.js
+++ script.js
@@ -1,8 +1,53 @@
 const numbersContainer = document.querySelector('.numbers')
+const rollButton = document.querySelector('.roll')
 
-for (let i = 1; i < 33; i++) {
+const allSectors = 32
+
+for (let i = 1; i <= allSectors; i++) {
   const sector = document.createElement('div')
   sector.classList.add('sector')
   sector.innerText = i
   numbersContainer.appendChild(sector)
 }
+
+function randomNumber() {
+  const timesHighlight = 50
+  const secondsInterval = 100
+  
+  let previous = document.querySelector('.win')
+
+  if (previous) {
+    previous.classList.remove('win')
+  }
+
+  const interval = setInterval(() => {
+    rollButton.removeEventListener('click', randomNumber)
+    
+    const randomSector = highlightSector()
+    randomSector.classList.add('win')
+
+    setTimeout(() => {
+      randomSector.classList.remove('win')
+    }, secondsInterval)
+  }, secondsInterval)
+
+  setTimeout(() => {
+    clearInterval(interval)
+
+    setTimeout(() => {
+      const randomSector = highlightSector()
+      randomSector.classList.add('win')
+
+      rollButton.addEventListener('click', randomNumber)
+    }, secondsInterval)
+  }, timesHighlight * secondsInterval)
+}
+
+function highlightSector() {
+  const sectors = document.querySelectorAll('.sector')
+  let randomIndex = Math.floor(Math.random() * sectors.length)
+
+  return sectors[randomIndex]
+}
+
+rollButton.addEventListener('click', randomNumber)
